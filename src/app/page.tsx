f5000625c64caf0fcb4abe6fe20c6524e55f30c9
arshidte <arshiddiyan.te.adte@gmail.com>
2023-09-14 23:46:06 +0530

    homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,47 @@
-import Image from 'next/image'
+import { Button } from '@/components/ui/button';
+import { UserButton, auth } from '@clerk/nextjs/app-beta';
+import { LogIn } from 'lucide-react';
+import Link from 'next/link';
 
-export default function Home() {
+export default async function Home() {
+  const { userId } = await auth();
+  const isAuth = !!userId;
   return (
-    <div className='text-red-600'>Hello World</div>
+    <div className='w-screen min-h-screen bg-gradient-to-r from-gray-700 via-gray-900 to-black'>
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <div className="flex flex-col items-center text-center">
+          
+          <div className="flex items-center">
+            <h1 className="mr-3 text-5xl font-semibold text-white">
+              Talk to your PDF
+            </h1>
+            <UserButton afterSignOutUrl='/' />
+          </div>
+
+          <div className="flex mt-8">
+            {isAuth && 
+              <Button variant={'secondary'}>Go to chats</Button>
+            }
+          </div>
+
+          <p className='max-w-xl mt-2 text-lg text-white'>
+            Get Quick, Accurate Responses to Your Questions, 
+            Revolutionizing the Way You Interact with Your Documents.
+          </p>
+
+          <div className="w-full mt-4">
+            {isAuth ? (<h1>fileupload</h1>):(
+              <Link href='/sign-in'>
+                <Button variant={'secondary'}>
+                  Login to get started!
+                  <LogIn className='w-4 h-4 ml-2'/>
+                </Button>
+              </Link>
+            )}
+          </div>
+
+        </div>
+      </div>
+    </div>
   )
 }
